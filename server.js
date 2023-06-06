c02fe863c1ed16030fdd3ea8ea397a82713f2313
lehelen19 <le.helen19@gmail.com>
2023-06-06 09:54:09 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+// CHECK TOKEN HERE
+
+// Add routes here
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+  console.log(`Express is listening on port ${port}!`);
+});
