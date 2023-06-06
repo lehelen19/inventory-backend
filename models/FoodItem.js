506d9f663ebe095d27e0a8330c5f84fbe5e20a2f
lehelen19 <le.helen19@gmail.com>
2023-06-06 10:03:18 -0700

    Create FoodItem model

---
+++ models/FoodItem.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const foodItemSchema = new Schema({
+  name: { type: String, required: true },
+  category: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+    },
+  ],
+  quantity: Number,
+});
+
+module.exports = mongoose.model('FoodItem', foodItemSchema);
